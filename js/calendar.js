32ace2405f8c634f80b11c4ee9584b289c653c82
seabrus <seabrus@gmail.com>
2015-01-27 22:49:26 +0200

    Version with Class - initial commit

--- js/calendar.js
+++ js/calendar.js
@@ -1,110 +1,21 @@
 /**
-   CALENDAR 5318, v.0.1.0
+   "Calendar 5318", v.0.1.2
    seabrus, https://github.com/seabrus
    MIT license
 */
 
-// ==================================
-//   CALENDAR events
-// ==================================
-
-$( document ).ready( function() {
-
-	// Open a calendar by a click on the calendar image
-	$('.calendar-img').on('click', function(event) {
-		var calendarDiv = $(this).nextAll('.calendar');
-
-		if ( calendarDiv.css('display') === 'none' ) {
-			var now = new Date();
-			calendarDiv.html( calcCalendar( now.getMonth(), now.getFullYear() ) );
-			calendarDiv.data( 'current-month', now.getMonth() ); 
-			calendarDiv.data( 'current-year', now.getFullYear() ); 
-			calendarDiv.css( 'display', 'block' ); 
-		}
-		else {
-			calendarDiv.html( '' );
-			calendarDiv.removeData( 'current-month' ); 
-			calendarDiv.removeData( 'current-year' ); 
-			calendarDiv.css( 'display', 'none' ); 
-		}
-
-		event.stopPropagation();
-	});
-
-	// This allows avoiding the closing of the calendar when clicking within it
-	$('.calendar').on('click', function(event) {
-		event.stopPropagation();
-	});
-
-	// Date picking out
-	$('.calendar').on('click', 'td', function(event) {
-		var inputElt = $(this).parents('.calendar').prevAll('input');
-		inputElt.val( $(this).attr('data-picked-day') );
-	});
-
-	// Recalculation after a month or year is changed
-	$('.calendar').on('click', '.change-calendar', function(event) {
-		var clickedButton = $(this);
-		var calendarDiv = clickedButton.parents('.calendar');
-
-		var month = calendarDiv.data( 'current-month' ) || 0,
-			   year   = calendarDiv.data( 'current-year' ) || 2015;
-
-		if ( clickedButton.hasClass('lessen-month') ) {
-			month -= 1;
-			if ( month === -1 ) {
-				month = 11; year -= 1;
-			}
-		}
-
-		if ( clickedButton.hasClass('add-month') ) {
-			month += 1;
-			if ( month === 12 ) {
-				month = 0; year += 1;
-			}
-		}
-
-		if ( clickedButton.hasClass('lessen-year') )
-			year -= 1;
-		if ( clickedButton.hasClass('add-year') )
-			year += 1;
-
-		if ( year < 1 ) year = 1;
-		if ( year > 4999 ) year = 4999;
-
-		calendarDiv.data( 'current-month', month ); 
-		calendarDiv.data( 'current-year', year ); 
-		calendarDiv.html( calcCalendar(month, year) );
-	});
-
-	// Closing when clicking on the modal dialog (except the calendar itself - see above)
-	$('#modal-add-dlg').on('click', function() {
-		var calendarDiv;
-		var calendarDivs = $('.calendar');   // There can be present more then one calendar <div> on a page
-
-		for( var i=0, len=calendarDivs.length; i< len; i++ ) {
-			calendarDiv = calendarDivs.eq(i);
-			if ( calendarDiv.css('display') === 'block' ) {
-				calendarDiv.html( '' );
-				calendarDiv.removeData( 'current-month' ); 
-				calendarDiv.removeData( 'current-year' ); 
-				calendarDiv.css( 'display', 'none' ); 
-			}
-		}
-	});
-
-
-});   /*===========   end of   $( document ).ready   ===========*/
-
-
+// Calendar constructor
+function Calendar() { };
+	
 // ===============================================
-// CALENDAR calculation and presentation
-// ===============================================
+// Main function: Calendar calculation and presentation
+// ==========================================================
 //    Parameters:
 //       mm 	- month to calculate the calendar for:   an integer, [0-11]
 //       yyyy    - year:   an integer, [1-4999]
-// ===============================================
-function calcCalendar( mm, yyyy ) {
+// ==========================================================
+Calendar.prototype.calcCalendar = function( mm, yyyy ) {
+
 	var 	weekdays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
 	var 	monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var 	monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -163,4 +74,88 @@ function calcCalendar( mm, yyyy ) {
 	return html;
 
 	function addZero(i) { return (i < 10 ? '0' : '') + i; }   // A function to format the date string: it adds zeros before numbers < 10
+}
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//     CALENDAR events
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// ========================
+// Show / hide the calendar
+// =============================================================
+//    Parameters:
+//       cDiv 		- jQuery object that specifies the calendar DIV -- like $('.calendar')
+// =============================================================
+Calendar.prototype.show = function( cDiv ) {
+
+	if ( cDiv.css('display') === 'none' ) {
+		var now = new Date();
+		cDiv.html( this.calcCalendar( now.getMonth(), now.getFullYear() ) );
+		cDiv.data( 'current-month', now.getMonth() ); 
+		cDiv.data( 'current-year', now.getFullYear() ); 
+		cDiv.css( 'display', 'block' ); 
+	}
+	else {
+		cDiv.html( '' );
+		cDiv.removeData( 'current-month' ); 
+		cDiv.removeData( 'current-year' ); 
+		cDiv.css( 'display', 'none' ); 
+	}
+}	
+
+// ========================
+// Change a month or year
+// =============================================================================
+//    Parameters:
+//       button 	- jQuery object that corresponds to the button clicked to change a month or year
+//       cDiv 		- jQuery object that specifies the calendar DIV -- like $('.calendar')
+// =============================================================================
+Calendar.prototype.changeMonthYear = function ( button, cDiv ) {
+	var month = cDiv.data( 'current-month' ) || 0,
+		   year   = cDiv.data( 'current-year' ) || 2015;
+
+	if ( button.hasClass('lessen-month') ) {
+		month -= 1;
+		if ( month === -1 ) {
+			month = 11; year -= 1;
+		}
+	}
+
+	if ( button.hasClass('add-month') ) {
+		month += 1;
+		if ( month === 12 ) {
+			month = 0; year += 1;
+		}
+	}
+
+	if ( button.hasClass('lessen-year') )
+		year -= 1;
+	if ( button.hasClass('add-year') )
+		year += 1;
+
+	if ( year < 1 ) year = 1;
+	if ( year > 4999 ) year = 4999;
+
+	cDiv.data( 'current-month', month ); 
+	cDiv.data( 'current-year', year ); 
+	cDiv.html( this.calcCalendar(month, year) );	
+}	
+
+// ========================
+// Close all opened calendars
+// =============================================================================
+Calendar.prototype.closeCalendar = function () {
+	var cDiv;
+	var cDivs = $('.calendar');   // There can be present more then one calendar <div> on a page
+
+	for( var i=0, len=cDivs.length; i< len; i++ ) {
+		cDiv = cDivs.eq(i);
+		if ( cDiv.css('display') === 'block' ) {
+			cDiv.html( '' );
+			cDiv.removeData( 'current-month' ); 
+			cDiv.removeData( 'current-year' ); 
+			cDiv.css( 'display', 'none' ); 
+		}
+	}
 }
--- js/init-modal.js
+++ js/init-modal.js
@@ -1,16 +1,53 @@
 $( document ).ready( function() {
 
-	// Initiation of the modal dialog window
-	$('#modal-add-dlg').on('show.bs.modal', function (event) {
+	// Initiation of the calendar
+    var calendar = new Calendar();
 
-		$('[href="#new-record-tab"]').css("display", "list-item"); 
-		$('[href="#new-record-tab"]').tab("show");
-
-		var now = new Date();
-		now = addZero(now.getDate()) + '-'  + addZero(now.getMonth() + 1) + '-' + now.getFullYear();
-		$('#buy-date').val( now );
-
-		function addZero(i) { return (i < 10 ? '0' : '') + i; }     // Helper function to format the Date string: It adds zeros before number < 10
+// =======================
+//     Calendar events
+// =======================
+	// Open a calendar by a click on the calendar icon
+	$('.calendar-img').on('click', function(event) {
+		var calendarDiv = $(this).nextAll('.calendar');
+		calendar.show( calendarDiv );
+		event.stopPropagation();
 	});
+	
+	// Date picking out
+	$('.calendar').on('click', 'td', function(event) {
+		var inputElt = $(this).parents('.calendar').prevAll('input');
+		inputElt.val( $(this).attr('data-picked-day') );
+	});
+	
+	// Recalculation after a month or year is changed
+	$('.calendar').on('click', '.change-calendar', function(event) {
+		var clickedButton = $(this);
+		var calendarDiv = clickedButton.parents('.calendar');
+		calendar.changeMonthYear( clickedButton, calendarDiv );
+	});
+
+	// Closing when clicking on the modal dialog (except the calendar itself - see below)
+	$('#modal-add-dlg').on('click', function() {
+		calendar.closeCalendar();
+	});
+
+	// This allows avoiding the closing of the calendar when clicking within it
+	$('.calendar').on('click', function(event) {
+		event.stopPropagation();
+	});
+
+
+
+/* ******************************************************************** */
+    // Initiation of the modal dialog window
+    $('#modal-add-dlg').on('show.bs.modal', function (event) {
+
+        var now = new Date();
+        now = addZero(now.getDate()) + '-'  + addZero(now.getMonth() + 1) + '-' + now.getFullYear();
+        $('#buy-date').val( now );
+
+        function addZero(i) { return (i < 10 ? '0' : '') + i; }     // Helper function to format the Date string: It adds zeros before number < 10
+    });
+   
 
 });
